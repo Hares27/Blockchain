57774259bb64c756aed3dad9500bb0ed7063d79a
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-11-29 08:29:30 +0530

    Update virtualmachine.js

--- virtualmachine.js
+++ virtualmachine.js
@@ -1,188 +1,144 @@
-
-
- function vm(data){
-     this.vmdata=data;
-     this.ip=0;
-     this.stack=new Stack() ;
-     this.contractState= {};
-     
- }
-
- 
-
-
-
-
-
- vm.prototype.runVm=function(){
-
-   this.data.forEach((instr)=>
-    {
-        
-        instruction=Instructions[instr];
-        
-        if(instruction){
-            this.executevminstruction(instruction);
-        }
-     this.ip++; 
-    })
+function vm(data) {
+  this.vmdata = data;
+  this.ip = 0;
+  this.stack = new Stack();
+  this.contractState = {};
 }
 
+vm.prototype.runVm = function () {
+  this.vmdata.forEach((instr) => {
+    instruction = Instructions[instr];
 
- vm.prototype.executevminstruction=function(instruction){
-     let a;
-     let b;
-     let c;
-     
-      switch(instruction){
-         
-          case "PUSH":
-             this.sp++;
-              this.stack.push(this.data[this.ip-1].toString());
-              
-              break;
-              
-          case "ADD":
-              a=Number(this.popStack());
-              b=Number(this.popStack());
-              c= a+b;
-              console.log(c);
-              this.sp++;
-              this.stack.push(c);
-              break;
+    if (instruction) {
+      this.executevminstruction(instruction);
+    }
+    this.ip++;
+  });
+};
 
-        case "Pushbytes":
-            this.stack.stackBytes.push(this.data[this.ip-1])
-            break;
+vm.prototype.getLastTwoElements = function () {
+  let a = Number(this.stack.pop());
+  let b = Number(this.stack.pop());
+  return { a: a, b: b };
+};
 
-        case "PackBytes":
-           
-             let packedByteString=this.getStringFromBytes();
+vm.prototype.executevminstruction = function (instruction) {
+  let lastTwoElements;
+  switch (instruction) {
+    case "PUSH":
+      this.sp++;
+      this.stack.push(this.vmdata[this.ip - 1].toString());
 
-             this.stack.push(packedByteString);
-             
-             this.stack.stackBytes=[];
+      break;
 
-             this.sp++;
-             break;
-        case "SUB":
+    case "ADD":
+      lastTwoElements = this.getLastTwoElements();
+      c = lastTwoElements.a + lastTwoElements.b;
+      this.sp++;
+      this.stack.push(c);
+      break;
 
-        a=Number(this.popStack());
-        b=Number(this.popStack());
-         c=a-b;
-        this.sp++;
-        this.stack.push(c);
-        break;
+    case "Pushbytes":
+      this.stack.stackBytes.push(this.data[this.ip - 1]);
+      break;
 
-        case "StoreState":
-            
-            
-            let key=this.stack.stack[this.stack.sp-1];
-           
-            let value=this.stack.stack[this.stack.sp-1];
+    case "PackBytes":
+      let packedByteString = this.getStringFromBytes();
 
-            this.contractState[key]=value;
-            
-            break;
+      this.stack.push(packedByteString);
 
-        case "getState":
+      this.stack.stackBytes = [];
 
-            let statekey=this.stack.stack[this.stack.sp-1];
-            
-             statevalue=this.contractState[statekey];
-           
-             this.popStack();
+      this.sp++;
+      break;
+    case "SUB":
+      lastTwoElements = this.getLastTwoElements();
+      c = lastTwoElements.a + lastTwoElements.b;
+      this.sp++;
+      this.stack.push(c);
+      break;
 
-             this.stack.push(statevalue);
+    case "StoreState":
+      let key = this.stack.stack[this.stack.sp - 1];
 
-             break;
+      let value = this.stack.stack[this.stack.sp - 1];
 
-        case "MUL":
-         a=Number(this.popStack());
-         b=Number(this.popStack());
-         c=b*a;
-        this.sp++;
-        this.stack.push(c);
-        break;
+      this.contractState[key] = value;
 
-      case "DIV":
-        
-        a=Number(this.popStack());
-        
-        b=Number(this.popStack());
-        c=a/b;   
-        this.sp++;
-        this.stack.push(c);
-        break;
+      break;
 
-             
-              
+    case "getState":
+      let statekey = this.stack.stack[this.stack.sp - 1];
 
-      }
- }
+      statevalue = this.contractState[statekey];
 
- vm.prototype.popStack=function(){
-     let value=this.stack.stack.pop();
-     console.log("value",value);
-     this.sp--;
-     return value;
- }
- vm.prototype.getStringFromBytes=function(){
-    let packedByteString="";
+      this.stack.pop();
 
-            
-    this.stack.stackBytes.forEach((byte)=>
-    {
-        packedByteString= packedByteString+byte.toString();
-    })
-    return packedByteString;
+      this.stack.push(statevalue);
 
- }
+      break;
 
+    case "MUL":
+      lastTwoElements = this.getLastTwoElements();
+      c = lastTwoElements.b * lastTwoElements.a;
+      this.sp++;
+      this.stack.push(c);
+      break;
 
+    case "DIV":
+      lastTwoElements = this.getLastTwoElements();
+      c = lastTwoElements.a / lastTwoElements.b;
+      this.sp++;
+      this.stack.push(c);
+      break;
+  }
+};
 
- function Stack(){
-    this.stack=[];
-    this.sp=0;
-    this.stackBytes=[];
-    
+vm.prototype.getStringFromBytes = function () {
+  let packedByteString = "";
+
+  this.stack.stackBytes.forEach((byte) => {
+    packedByteString = packedByteString + byte.toString();
+  });
+  return packedByteString;
+};
+
+function Stack() {
+  this.stack = [];
+  this.sp = 0;
+  this.stackBytes = [];
 }
 
-Stack.prototype.push=function(number){
-    this.stack.push(number);
-    this.sp++;
-    console.log(this.stack);
+Stack.prototype.push = function (number) {
+  this.stack.push(number);
+  this.sp++;
+};
 
-}
+Stack.prototype.pop = function () {
+  let value = this.stack.pop();
+  this.sp--;
+  return value;
+};
 
-Stack.prototype.pop=function(){
+const Instructions = {
+  "0x0a": "PUSH",
+  "0x0b": "ADD",
+  "0x0c": "Pushbytes",
+  "0x0d": "PackBytes",
+  "0x0e": "SUB",
+  "0x0f": "StoreState",
+  "0x0g": "getState",
+  "0x0h": "MUL",
+  "0x0i": "DIV",
+};
 
-    this.stack.pop();
-    this.sp--;
-    console.log(this.stack);
-
-}
-
- 
-
-
-
- const Instructions={"0x0a" : "PUSH","0x0b":"ADD","0x0c":"Pushbytes","0x0d":"PackBytes","0x0e":"SUB","0x0f":"StoreState","0x0g":"getState","0x0h":"MUL","0x0i":"DIV"};
-
- // Testing pushBytes
+// Testing pushBytes
 //  const newvm=new vm([2,"0x0a",(Buffer.from("a","utf-8")),"0x0c",(Buffer.from("b")),"0x0c","0x0d","0x0f",(Buffer.from("a","utf-8")),"0x0c",Buffer.from("b"),"0x0c","0x0d","0x0g"]);
- 
 
- // Testing arithmetic operations
-const newvm=new vm([2,"0x0a",4,"0x0a","0x0b"])
- newvm.runVm();
+// Testing arithmetic operations
+const newvm = new vm([2, "0x0a", 4, "0x0a", "0x0b"]);
+newvm.runVm();
 //  console.log(newvm.contractState);
 //  console.log(newvm.stack.stack);
- 
 
-
- // How can we push bytes
-
-
-
- 
+// How can we push bytes
